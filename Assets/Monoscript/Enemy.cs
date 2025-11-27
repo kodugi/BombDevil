fa2b1314930af80b07841df62aef40957ffaae36
younggichoi <choi_younggi@naver.com>
2025-11-27 13:49:58 +0900

    knockback at boundary
    경계를 통과하는 넉백 구현

--- Assets/Monoscript/Enemy.cs
+++ Assets/Monoscript/Enemy.cs
@@ -7,12 +7,19 @@ public class Enemy : MonoBehaviour
     // motion duration (get from GameManager)
     private float _walkDuration;
     private float _knockbackDuration;
+    
+    // boundary coordinate (get from GameManager)
+    private float _minX, _maxX, _minY, _maxY;
 
     // initializing internal attribute
     void Awake()
     {
         _walkDuration = GameManager.Instance.walkDuration;
         _knockbackDuration = GameManager.Instance.knockbackDuration;
+        _minX = GameManager.GetMinX();
+        _maxX = GameManager.GetMaxX();
+        _minY = GameManager.GetMinY();
+        _maxY = GameManager.GetMaxY();
     }
     
     // walk API
@@ -38,11 +45,39 @@ public class Enemy : MonoBehaviour
         {
             time += Time.deltaTime;
             float t = time / duration;
-            transform.position = Vector3.Lerp(start, target, t);
+            transform.position = LerpWrap(start, target, t, _minX, _maxX, _minY, _maxY);
             yield return null;
         }
 
-        transform.position = target;
+        transform.position = GetWrappedTarget(direction, distance, start, _minX, _maxX, _minY, _maxY);
+    }
+
+    // It can process boundary condition
+    private static Vector3 LerpWrap(Vector3 a, Vector3 b, float t,
+        float minX = float.NegativeInfinity, float maxX = float.PositiveInfinity,
+        float minY = float.NegativeInfinity, float maxY = float.PositiveInfinity,
+        float minZ = float.NegativeInfinity, float maxZ = float.PositiveInfinity)
+    {
+        float x = a.x + (b.x - a.x) * t;
+        float y = a.y + (b.y - a.y) * t;
+        float z = a.z + (b.z - a.z) * t;
+        
+        if (x > maxX)
+            x = minX + (b.x - a.x) * t + maxX - a.x;
+        else if (x < minX)
+            x = maxX + (b.x - a.x) * t - minX + a.x;
+        
+        if (y > maxY)
+            y = minY + (b.y - a.y) * t + maxY - a.y;
+        else if (y < minY)
+            y = maxY + (b.y - a.y) * t - minY + a.y;
+        
+        if (z > maxZ)
+            z = minZ + (b.z - a.z) * t + maxX - a.z;
+        else if (x < minX)
+            z = maxX + (b.z - a.z) * t - minX + a.z;
+
+        return new Vector3(x, y, z);
     }
 
     // get destination from direction, distance, start point
@@ -76,5 +111,39 @@ public class Enemy : MonoBehaviour
         }
 
         return new Vector3();
+    }
+    
+    // get destination wrt boundary condition
+    private static Vector3 GetWrappedTarget(Direction direction, int distance, Vector3 start,
+        float minX = float.NegativeInfinity, float maxX = float.PositiveInfinity,
+        float minY = float.NegativeInfinity, float maxY = float.PositiveInfinity,
+        float minZ = float.NegativeInfinity, float maxZ = float.PositiveInfinity)
+    {
+
+        Vector3 result = GetTarget(direction, distance, start);
+        
+        if (result.x < minX || result.x > maxX)
+            result.x = Mod(result.x - minX, maxX - minX) + minX;
+        if (result.y < minY || result.y > maxY)
+            result.y = Mod(result.y - minY, maxY - minY) + minY;
+        if (result.z < minZ || result.z > maxZ)
+            result.z = Mod(result.z - minZ, maxZ - minZ) + minZ;
+
+        return result;
+    }
+
+    private static float Mod(float x, int m)
+    {
+        return (x % m + m) % m;
+    }
+    
+    private static float Mod(float x, float m)
+    {
+        return (x % m + m) % m;
+    }
+    
+    private static int Mod(int x, int m)
+    {
+        return (x % m + m) % m;
     }
 }
--- Assets/Monoscript/GameManager.cs
+++ Assets/Monoscript/GameManager.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class GameManager : MonoBehaviour
 {
@@ -27,12 +29,19 @@ public class GameManager : MonoBehaviour
     
     // tracking auxiliary bomb order
     private List<Vector2Int> _auxiliaryBombs;
+    
+    // boundary coordinate
+    private static float _minX, _minY, _maxX, _maxY;
 
     void Awake()
     {
         Instance = this;
         _board = new GameObject[width, height];
         _auxiliaryBombs = new List<Vector2Int>();
+        _minX = -width / 2.0f;
+        _maxX = width / 2.0f;
+        _minY = -height / 2.0f;
+        _maxY = height / 2.0f;
     }
 
     void Update()
@@ -95,58 +104,157 @@ public class GameManager : MonoBehaviour
             Knockback(x, y);
         }
     }
+    
+    // get boundary coordinate API
+    public static float GetMaxX()
+    {
+        return _maxX;
+    }
+    
+    public static float GetMinX()
+    {
+        return _minX;
+    }
+    
+    public static float GetMaxY()
+    {
+        return _maxY;
+    }
+    
+    public static float GetMinY()
+    {
+        return _minY;
+    }
 
     // find enemy nearby (x,y) and push them (call Enemy.Knockback)
     private void Knockback(int x, int y)
     {
         //Up
-        if (_board[x, (y + 1)%height]
-                           && _board[x, (y + 1)%height].GetComponent<Enemy>() != null)
+        if (_board[x, Mod(y + 1, height)]
+                           && _board[x, Mod(y + 1, height)].GetComponent<Enemy>() != null)
         {
-            _board[x, (y + 1)%height].GetComponent<Enemy>().Knockback(Direction.Up, knockbackDistance);
+            _board[x, Mod(y + 1, height)].GetComponent<Enemy>().Knockback(Direction.Up, knockbackDistance);
+            ReflectMoveInBoard(x, Mod(y + 1, height), Direction.Up, knockbackDistance);
         }
         //Down
-        if (_board[x, ((y - 1)%height + height)%height]
-                  && _board[x, ((y - 1)%height + height)%height].GetComponent<Enemy>() != null)
+        if (_board[x, Mod(y - 1, height)]
+                  && _board[x, Mod(y - 1, height)].GetComponent<Enemy>() != null)
         {
-            _board[x, ((y - 1)%height + height)%height].GetComponent<Enemy>().Knockback(Direction.Down, knockbackDistance);
+            _board[x, Mod(y - 1, height)].GetComponent<Enemy>().Knockback(Direction.Down, knockbackDistance);
+            ReflectMoveInBoard(x, Mod(y - 1, height), Direction.Down, knockbackDistance);
         }
         //Right
-        if (_board[(x + 1)%width, y]
-                          && _board[(x + 1)%width, y].GetComponent<Enemy>() != null)
+        if (_board[Mod(x + 1, width), y]
+                          && _board[Mod(x + 1, width), y].GetComponent<Enemy>() != null)
         {
-            _board[(x + 1)%width, y].GetComponent<Enemy>().Knockback(Direction.Right, knockbackDistance);
+            _board[Mod(x + 1, width), y].GetComponent<Enemy>().Knockback(Direction.Right, knockbackDistance);
+            ReflectMoveInBoard(Mod(x + 1, width), y, Direction.Right, knockbackDistance);
         }
         //Left
-        if (_board[((x - 1)%width + width)%width, y]
-                  && _board[((x - 1)%width + width)%width, y].GetComponent<Enemy>() != null)
+        if (_board[Mod(x - 1, width), y]
+                  && _board[Mod(x - 1, width), y].GetComponent<Enemy>() != null)
         {
-            _board[((x - 1)%width + width)%width, y].GetComponent<Enemy>().Knockback(Direction.Left, knockbackDistance);
+            _board[Mod(x - 1, width), y].GetComponent<Enemy>().Knockback(Direction.Left, knockbackDistance);
+            ReflectMoveInBoard(Mod(x - 1, width), y, Direction.Left, knockbackDistance);
         }
         //UpRight
-        if (_board[(x + 1)%width, (y + 1)%height]
-            && _board[(x + 1)%width, (y + 1)%height].GetComponent<Enemy>() != null)
+        if (_board[Mod(x + 1, width), Mod(y + 1, height)]
+            && _board[Mod(x + 1, width), Mod(y + 1, height)].GetComponent<Enemy>() != null)
         {
-            _board[(x + 1)%width, (y + 1)%height].GetComponent<Enemy>().Knockback(Direction.UpRight, knockbackDistance);
+            _board[Mod(x + 1, width), Mod(y + 1, height)].GetComponent<Enemy>().Knockback(Direction.UpRight, knockbackDistance);
+            ReflectMoveInBoard(Mod(x + 1, width), Mod(y + 1, height), Direction.UpRight, knockbackDistance);
         }
         //DownLeft
-        if (_board[((x - 1)%width + width)%width, ((y - 1)%height + height)%height]
-            && _board[((x - 1)%width + width)%width, ((y - 1)%height + height)%height].GetComponent<Enemy>() != null)
+        if (_board[Mod(x - 1, width), Mod(y - 1, height)]
+            && _board[Mod(x - 1, width), Mod(y - 1, height)].GetComponent<Enemy>() != null)
         {
-            _board[((x - 1)%width + width)%width, ((y - 1)%height + height)%height].GetComponent<Enemy>().Knockback(Direction.DownLeft, knockbackDistance);
+            _board[Mod(x - 1, width), Mod(y - 1, height)].GetComponent<Enemy>().Knockback(Direction.DownLeft, knockbackDistance);
+            ReflectMoveInBoard(Mod(x - 1, width), Mod(y - 1, height), Direction.DownLeft, knockbackDistance);
         }
         //DownRight
-        if (_board[(x + 1)%width, ((y - 1)%height + height)%height]
-            && _board[(x + 1)%width, ((y - 1)%height + height)%height].GetComponent<Enemy>() != null)
+        if (_board[Mod(x + 1, width), Mod(y - 1, height)]
+            && _board[Mod(x + 1, width), Mod(y - 1, height)].GetComponent<Enemy>() != null)
         {
-            _board[(x + 1)%width, ((y - 1)%height + height)%height].GetComponent<Enemy>().Knockback(Direction.DownRight, knockbackDistance);
+            _board[Mod(x + 1, width), Mod(y - 1, height)].GetComponent<Enemy>().Knockback(Direction.DownRight, knockbackDistance);
+            ReflectMoveInBoard(Mod(x + 1, width), Mod(y - 1, height), Direction.DownRight, knockbackDistance);
         }
         //UpLeft
-        if (_board[((x - 1)%width + width)%width, (y + 1)%height]
-            && _board[((x - 1)%width + width)%width, (y + 1)%height].GetComponent<Enemy>() != null)
+        if (_board[Mod(x - 1, width), Mod(y + 1, height)]
+            && _board[Mod(x - 1, width), Mod(y + 1, height)].GetComponent<Enemy>() != null)
         {
-            _board[((x - 1)%width + width)%width, (y + 1)%height].GetComponent<Enemy>().Knockback(Direction.UpLeft, knockbackDistance);
+            _board[Mod(x - 1, width), Mod(y + 1, height)].GetComponent<Enemy>().Knockback(Direction.UpLeft, knockbackDistance);
+            ReflectMoveInBoard(Mod(x - 1, width), Mod(y + 1, height), Direction.UpLeft, knockbackDistance);
         }
+    }
+    
+    // update board when object movement occurs
+    private void ReflectMoveInBoard(int x, int y, Direction direction, int distance)
+    {
+        GameObject obj = _board[x, y];
+        if (obj == null)
+            return;
+
+        Vector3 target = GetWrappedTarget(direction, distance, new Vector3(x, y, 0), 0, width, 0, height);
+        if (_board[(int)target.x, (int)target.y] != null)
+        {
+            // collision
+        }
+
+        _board[(int)target.x, (int)target.y] = obj;
+        _board[x, y] = null;
+
+    }
+    
+    // get destination from direction, distance, start point
+    private static Vector3 GetTarget(Direction direction, int distance, Vector3 start)
+    {
+        switch (direction)
+        {
+            case Direction.Up:
+                return start + new Vector3(0, distance, 0);
+            
+            case Direction.Down:
+                return start - new Vector3(0, distance, 0);
+            
+            case Direction.Right:
+                return start + new Vector3(distance, 0, 0);
+            
+            case Direction.Left:
+                return start - new Vector3(distance, 0, 0);
+            
+            case Direction.UpRight:
+                return start + new Vector3(distance, distance, 0);
+            
+            case Direction.UpLeft:
+                return start + new Vector3(-distance, distance, 0);
+            
+            case Direction.DownRight:
+                return start + new Vector3(distance, -distance, 0);
+            
+            case Direction.DownLeft:
+                return start - new Vector3(distance, distance, 0);
+        }
+
+        return new Vector3();
+    }
+    
+    // get destination wrt boundary condition
+    private static Vector3 GetWrappedTarget(Direction direction, int distance, Vector3 start,
+        float minX = float.NegativeInfinity, float maxX = float.PositiveInfinity,
+        float minY = float.NegativeInfinity, float maxY = float.PositiveInfinity,
+        float minZ = float.NegativeInfinity, float maxZ = float.PositiveInfinity)
+    {
+
+        Vector3 result = GetTarget(direction, distance, start);
+        
+        if (result.x < minX || result.x >= maxX)
+            result.x = Mod(result.x - minX, maxX - minX) + minX;
+        if (result.y < minY || result.y >= maxY)
+            result.y = Mod(result.y - minY, maxY - minY) + minY;
+        if (result.z < minZ || result.z >= maxZ)
+            result.z = Mod(result.z - minZ, maxZ - minZ) + minZ;
+
+        return result;
     }
 
     // when mouse click occurs on the board, plant auxiliary bomb in the cell
@@ -183,5 +291,20 @@ public class GameManager : MonoBehaviour
     private int GlobalToGridY(float y)
     {   
         return Mathf.FloorToInt(y + height / 2f);
+    }
+
+    private static float Mod(float x, int m)
+    {
+        return (x % m + m) % m;
+    }
+    
+    private static float Mod(float x, float m)
+    {
+        return (x % m + m) % m;
+    }
+    
+    private static int Mod(int x, int m)
+    {
+        return (x % m + m) % m;
     }
 }
